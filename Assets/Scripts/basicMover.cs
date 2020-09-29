53f12f6c9d34218fd8e24615c6320a299f19b096
Cameron Mellott <cameroncmellott@gmail.com>
2020-09-29 11:38:01 -0700

    getting a camera to follow a box pretty well
    camera now follows box smoothly, edges still can pop in on screen

---
+++ Assets/Scripts/basicMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicMover : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dir = Input.GetAxis("Horizontal");
+        Vector3 desiredPosition = transform.position + (new Vector3(moveSpeed, 0, 0) * dir);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime);
+        transform.position = smoothPosition;
+    }
+}
---
+++ Assets/Scripts/simpleCameraMover.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class simpleCameraMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = gameObject.GetComponent<Camera>();
+        mainCamera.velocity.Set(100f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.velocity.Set(100f, 0f, 0f);
+
+        Debug.Log(mainCamera.velocity);
+    }
+}
