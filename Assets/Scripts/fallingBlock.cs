61842edae73b5bb6d38edd0f8915ad4aac956011
Cameron Mellott <cameroncmellott@gmail.com>
2020-10-03 15:59:47 -0700

    added basic block fall and time slow scene

---
+++ Assets/Scripts/fallingBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingBlock : MonoBehaviour
+{
+    private Vector3 spawnLocation;
+    [SerializeField] private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -6f)
+        {
+            transform.position = spawnLocation;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/timeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Time.timeScale = 0.05f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = 0.02f;
+        }
+    }
+}
