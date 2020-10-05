e2c529a26a35436c5ba7cb6372f0949d5805d9e0
Cameron Mellott <cameroncmellott@gmail.com>
2020-10-05 12:10:48 -0700

    Tweeked time slow to not slow down player

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,27 +1,29 @@
-﻿using System;
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fallingBlock : MonoBehaviour
+public class player : MonoBehaviour
 {
+    private bool slow = false;
+    private float speed = 0f;
     private Vector3 spawnLocation;
-    [SerializeField] private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         spawnLocation = transform.position;
     }
-
     // Update is called once per frame
     void Update()
     {
+        speed += 0.05f * Time.unscaledDeltaTime;
         if (transform.position.y < -6f)
         {
             transform.position = spawnLocation;
-            rb.velocity = new Vector3(0f, 0f, 0f);
+            speed = 0f;
         }
 
+        transform.position += new Vector3(0f, -1f, 0f) * speed;
     }
 }
-
